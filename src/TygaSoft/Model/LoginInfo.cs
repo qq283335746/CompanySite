b41cca46e694052565ca3b94f12ea30af2920750
qq283335746 <283335746@qq.com>
2019-07-17 16:09:17 +0800

    add .gitignore

---
+++ src/TygaSoft/Model/LoginInfo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TygaSoft.Model
+{
+    public class LoginInfo
+    {
+        public LoginInfo() { }
+
+        public LoginInfo(string username,DateTime lastUpdatedDate)
+        {
+            UserName = username;
+            LastUpdatedDate = lastUpdatedDate;
+        }
+
+        public string UserName { get; set; }
+
+        public DateTime LastUpdatedDate { get; set; }
+    }
+}
---
+++ src/TygaSoft/WebHelper/FileHelper.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace TygaSoft.WebHelper
+{
+    public class FileHelper
+    {
+        public static string GetFullPath(string path)
+        {
+            return Path.GetFullPath(path);
+        }
+    }
+}
